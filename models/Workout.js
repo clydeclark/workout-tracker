78ac3897c60cac30bca3cc1a00c1c45d6a967084
Clyde Clark <clydeclark78@gmail.com>
2020-11-05 12:12:36 -0800

    update Workout.js formatting to match seed.js

--- models/Workout.js
+++ models/Workout.js
@@ -3,24 +3,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    type: {
-        type: String
+    day: {
+        type: Date
     },
-    name: {
-        type: String
-    },
-    duration: {
-        type: Number
-    },
-    weight: {
-        type: Number
-    },
-    reps: {
-        type: Number
-    },
-    sets: {
-        type: Number
-    }
+    exercises: [{
+        type: {
+            type: String
+        },
+        name: {
+            type: String
+        },
+        duration: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        }
+    }]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
